47e93fcd892ceae62cb8468b3f6be6ce60b6c520
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-09 14:36:04 +0530

    final functionality done

--- src/App.js
+++ src/App.js
@@ -16,23 +16,15 @@ function DateCounter() {
   const date = new Date("June 9 2024");
   date.setDate(date.getDate() + currentValue);
 
-  function handleScroll(event) {
-    setRange(parseFloat(event.target.value));
+  function handleReset() {
+    setCurrentvalue(0);
+    setRange(1);
   }
 
-  function handleClick(v) {
-    if (range >= 1) {
-      v
-        ? setCurrentvalue(currentValue + range)
-        : setCurrentvalue(currentValue - range);
-    }
+  function handleChange(value) {
+    setCurrentvalue(value !== "" ? value : 0);
   }
 
-  function handleChange(event) {
-    setCurrentvalue(
-      parseFloat(event.target.value !== "" ? event.target.value : 0)
-    );
-  }
   return (
     <main>
       <div className="scroller">
@@ -41,21 +33,33 @@ function DateCounter() {
           type="range"
           min={1}
           max={10}
-          onChange={handleScroll}
+          onChange={(e) => setRange(Number(e.target.value))}
           defaultValue={1}
         />
         <label>{range}</label>
       </div>
       <div className="value-box">
-        <button onClick={() => handleClick()}>-</button>
+        <button
+          onClick={() =>
+            range >= 1 ? setCurrentvalue(currentValue - range) : currentValue
+          }
+        >
+          -
+        </button>
         <input
           className="count-box"
           value={currentValue}
           type="text"
-          onChange={handleChange}
+          onChange={(e) => handleChange(Number(e.target.value))}
           defaultValue={0}
         />
-        <button onClick={() => handleClick(true)}>+</button>
+        <button
+          onClick={() =>
+            range >= 1 ? setCurrentvalue(currentValue + range) : currentValue
+          }
+        >
+          +
+        </button>
       </div>
       <div className="date-box">
         <p>
@@ -67,11 +71,13 @@ function DateCounter() {
           {date.toDateString()}
         </p>
       </div>
-      <div className="reset">
-        <button className="reset-button" onClick={() => setCurrentvalue(0)}>
-          Reset
-        </button>
-      </div>
+      {currentValue !== 0 ? (
+        <div className="reset">
+          <button className="reset-button" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      ) : null}
     </main>
   );
 }
